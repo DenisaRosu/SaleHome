301e44b3fc890de4518a277abdf5ba8acb19a2e0
Denisa <rosu_rebeca_denisa@yahoo.com>
2022-05-19 14:21:10 +0300

    19 may

--- pages/design.js
+++ pages/design.js
@@ -1,3 +1,5 @@
+import Search from "../pages/search"
+
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import InfoCard from "../components/InfoCard"
@@ -14,52 +16,33 @@ import {SearchIcon,
 
 
 
-function Search({searchResults}) {
+function Design({designResults}) {
    
 
-const [noOfBedRooms, setNoOfBedRooms]=useState(1);
-const [noOfBathRooms, setNoOfBathRooms]=useState(1);
-const [noOfMP, setNoOfMPs]=useState(1);
 
 const router = useRouter();
-const save = ()=>{
-  // router.push('/search')
-  router.push({
-    pathname:'/design',
-    query: {
-       noOfBedRooms,
-        noOfBathRooms,
-         noOfMP,
-         location, 
-         noOfRooms,
-
-      
-    }
-  })
-  }
 
    //ES6 destructuring 
   
-   const{startDate, location, noOfRooms}  = router.query;
+   const{noOfBedRooms,
+        noOfBathRooms,
+         noOfMP,
+         location, 
+         noOfRooms}  = router.query;
 
-   //const filtered = router.query.filter(noOfRooms ===)
-    
-  //const sameNoOfRooms = searchResults.filter(searchResult => searchResult.noRooms === noOfRooms);
-  //console.log(sameNoOfRooms);
+ 
 
-
-   const formattedStartDate = format(new Date(startDate),"dd MMMM yy");  //install npm install --save react date-fns ( may crash again ?! ) 
+ 
+   //const formattedStartDate = format(new Date(startDate),"dd MMMM yy");  //install npm install --save react date-fns ( may crash again ?! ) 
    return  (
 
        <div>
-           <Header placeholder={`${location} | ${formattedStartDate}| ${noOfRooms} rooms`}/>
+           <Header placeholder={`${location}| ${noOfRooms} rooms`}/>
 
            
            <main className=' text-blue-800'>
               <section >
-              <p className="text-xm py-3">Meet with us on {formattedStartDate}</p>
-              <p className="text-xm py-3">Number of possible houses to be done on {formattedStartDate}</p>
-             <h1 className="text-3xl py-3 font-semibold">Houses in {location} with {noOfRooms} rooms</h1>
+                <h1 className="text-3xl py-3 font-semibold">Houses in {location} with {noOfRooms} rooms</h1>
              
                 <div className="px-4 py-2 space-y-3 flex-grow " >
                 <h2 className="flex-grow text-2xl py-3 flex items-center ">Number of bedrooms
@@ -88,17 +71,18 @@ const save = ()=>{
                     value={noOfMP}
                     onChange={(e)=>setNoOfMPs(e.target.value)}
                     type="number" 
-                    min={1}
+                    min={30}
                     className="input"/>
 
                 </p>
             
 
-                 <button onClick={save} className="">SEARCH!</button>
                 
                 </div>
                
-                  {searchResults.filter(searchResult => searchResult.noRooms === noOfRooms && searchResult.zona === location ).map(({img, mp, zona, pret, noRooms, noBath}) => (   
+                  {designResults
+                  .filter(designResult => designResult.mp === noOfMP &&designResult.noRooms  === noOfRooms &&designResult.zona === location )
+                  .map(({img, mp, zona, pret, noRooms, noBath}) => (   
            
                       <InfoCard
                 
@@ -123,16 +107,16 @@ const save = ()=>{
    )
 }
 
-export default Search 
+export default Design
 
 export async function getServerSideProps() {
-    const searchResults = await fetch("http://localhost:3000/offersAparts.json").
+    const designResults = await fetch("http://localhost:3000/offersAparts.json").
     then(res => res.json()
     );
     
     return {
         props: {
-            searchResults,
+            designResults,
         },
     };
 
